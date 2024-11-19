79bcb2ae91dff20240678831f0c21c143eacfc1e
ksks2012 <st890751999@gmail.com>
2024-11-20 01:19:51 +0800

    feat(run_flow):
    - Implement run_genai_chat_api function to handle chat interactions
    - Skip step of translator

--- run/run_flow.py
+++ run/run_flow.py
@@ -1,21 +1,12 @@
 
 from utils.api.basic import GenaiAPI
-from utils.translator import TranslatorZhTwToEn
+from utils.api.chat import GenaiChatAPI
 from utils.file_processor import read_json, read_yaml
 
 def run_genai_api():
     # TODO: Replace test file with crawler data
     data = read_json("./var/test.json")
     print(data)
-
-    translator = TranslatorZhTwToEn()
-    for value in data:
-        # Translate title and message
-        translated_title = translator.translate(value["title"])
-        translated_message = translator.translate(value["message"])
-
-        value["translated_title"] = translated_title
-        value["translated_message"] = translated_message
 
     config = read_yaml("./etc/config.yaml")
     genai_api = GenaiAPI(config)
@@ -25,8 +16,18 @@ def run_genai_api():
         print(response.text)
 
 def run_genai_chat_api():
-    pass
+    # TODO: Replace test file with crawler data
+    data = read_json("./var/test.json")
+    print(data)
+
+    config = read_yaml("./etc/config.yaml")
+    genai_api = GenaiChatAPI(config)
+    genai_api.start_chat()
+
+    for value in data:
+        response = genai_api.send_msg_to_chat(value["translated_title"], value["translated_message"])
+        print(response.text)
 
 if __name__ == "__main__":
-    run_genai_api()
+    # run_genai_api()
     run_genai_chat_api()
